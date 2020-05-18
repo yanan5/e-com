f21d90d4b823e1eea78b0ce06ebc2053a5883799
yanan5 <obrnarayanan@gmail.com>
2020-05-18 10:12:36 +0530

    added styles for navbar search

--- e-comm-full-stack/src/app-ui-comp/TopBar/TopBar.js
+++ e-comm-full-stack/src/app-ui-comp/TopBar/TopBar.js
@@ -7,32 +7,17 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Badge from "@material-ui/core/Badge";
 import MenuIcon from "@material-ui/icons/Menu";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
+import Grid from "@material-ui/core/Grid";
+
 import LocalMallOutlinedIcon from "@material-ui/icons/LocalMallOutlined";
+
+import { ExpandableSearch } from "../../base-ui-comp";
 import "./TopBar.scss";
 
 export default function PrimarySearchAppBar() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
+  const matches = useMediaQuery("(max-width:980px)");
 
   return (
     <div className="AppBar">
@@ -41,32 +26,111 @@ export default function PrimarySearchAppBar() {
           <Typography className="title" variant="h2">
             SHOPMATE
           </Typography>
-          <Box>
-            <Button>Women</Button>
-            <Button>Men</Button>
-            <Button>Kids</Button>
-            <Button>Shoes</Button>
-            <Button>Brands</Button>
+          {/* {!matches && (
+            <> */}
+              <nav className="mainNav">
+                <ul>
+                  <li>
+                    <Button>Women</Button>
+                    <Grid container className="mega-menu">
+                      <div class="arrow-up"></div>
+                      <Grid item xs={12} sm={3}>
+                        <ul>
+                          <li>Shoes</li>
+                          <li>Sneakers & Canvas</li>
+                          <li>Sandals</li>
+                          <li>Formal Shoes</li>
+                          <li>Boots</li>
+                          <li>Causal Shoes</li>
+                        </ul>
+                      </Grid>
+                      <Grid item xs={12} sm={3}>
+                        <ul>
+                          <li>Clothing</li>
+                          <li>T-Shirts, Vests & Pools</li>
+                          <li>Shorts</li>
+                          <li>Shirts</li>
+                          <li>Jeans</li>
+                          <li>Trousers & Chinos</li>
+                          <li>Coats, Jackets & Gliets</li>
+                          <li>Sweatshirts</li>
+                          <li>Underwear & Baselayers</li>
+                        </ul>
+                      </Grid>
+                      <Grid item xs={12} sm={3}>
+                        <ul>
+                          <li>Accessories</li>
+                          <li>Watches</li>
+                          <li>Bags & Wallets</li>
+                          <li>Sunglasses & Eyewear</li>
+                          <li>Hats & Caps</li>
+                          <li>Socks</li>
+                          <li>Jewellery</li>
+                          <li>Belts</li>
+                          <li>Formal & Accessories</li>
+                        </ul>
+                      </Grid>
+                      <Grid item xs={12} sm={3}>
+                        <ul>
+                          <li>Accessories</li>
+                          <li>Sports Equipment</li>
+                          <li>Sports Shoes</li>
+                          <li>Sports Clothing</li>
+                          <li>Hats & Caps</li>
+                        </ul>
+                        <ul>
+                          <li>Grooming</li>
+                          <li>Skincare</li>
+                          <li>Shaving</li>
+                          <li>Hair Care</li>
+                        </ul>
+                      </Grid>
+                    </Grid>
+                  </li>
+                  <li>
+                    <Button>Men</Button>
+                  </li>
+                  <li>
+                    <Button>Kids</Button>
+                  </li>
+                  <li>
+                    <Button>Shoes</Button>
+                  </li>
+                  <li>
+                    <Button>Brands</Button>
+                  </li>
+                </ul>
+              </nav>
+              <Box className="expandableSearchWrapper">
+                <ExpandableSearch
+                  onSearch={(v) => {
+                    console.log(v);
+                  }}
+                />
+              </Box>
+              <IconButton
+                edge="end"
+                aria-label="account of current user"
+                aria-haspopup="true"
+                onClick={() => {}}
+                color="inherit"
+              >
+                <Badge badgeContent={4} color="secondary">
+                  <LocalMallOutlinedIcon />
+                </Badge>
+              </IconButton>
+            {/* </>
+          )} */}
+          {matches && (
             <IconButton
-              edge="end"
-              aria-label="account of current user"
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
+              edge="start"
+              className="menuButton"
               color="inherit"
+              aria-label="open drawer"
             >
-              <Badge badgeContent={4} color="secondary">
-                <LocalMallOutlinedIcon />
-              </Badge>
+              <MenuIcon fontSize="large" />
             </IconButton>
-          </Box>
-          <IconButton
-            edge="start"
-            className="menuButton"
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon fontSize="large" />
-          </IconButton>
+          )}
         </Toolbar>
       </AppBar>
     </div>
